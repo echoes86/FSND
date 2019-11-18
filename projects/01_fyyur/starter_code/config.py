9035046671e9687ca59c116807ce13bde17775e2
Simone Fusco <simone.fusco@ennova.it>
2019-11-18 22:53:00 +0100

    Project Fyyur

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -10,4 +10,6 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+# done
+SQLALCHEMY_DATABASE_URI = 'postgresql://xxx:postgres@localhost:5432/fyyur'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
