984a301eff153669c9b74b9b6496208976c04942
Simone Fusco <simone.fusco@ennova.it>
2019-12-19 15:45:55 +0100

    pagination and GET questions init

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -15,13 +15,13 @@ def create_app(test_config=None):
     setup_db(app)
 
     '''
-  @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
-  '''
+    @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
+    '''
     CORS(app)
 
     '''
-  @TODO: Use the after_request decorator to set Access-Control-Allow
-  '''
+    @TODO: Use the after_request decorator to set Access-Control-Allow
+    '''
 
     @app.after_request
     def after_request_handler(response):
@@ -35,85 +35,108 @@ def create_app(test_config=None):
         return response
 
     '''
-  @TODO: 
-  Create an endpoint to handle GET requests 
-  for all available categories.
-  '''
-
-    @app.route('/category/<id>', methods=['GET'], defaults={'id': None})
-    def categories(id):
-
-
-
-
-  '''
-  @TODO: 
-  Create an endpoint to handle GET requests for questions, 
-  including pagination (every 10 questions). 
-  This endpoint should return a list of questions, 
-  number of total questions, current category, categories. 
-
-  TEST: At this point, when you start the application
-  you should see questions and categories generated,
-  ten questions per page and pagination at the bottom of the screen for three pages.
-  Clicking on the page numbers should update the questions. 
-  '''
-
+    @TODO: 
+    Create an endpoint to handle GET requests 
+    for all available categories.
     '''
-  @TODO: 
-  Create an endpoint to DELETE question using a question ID. 
+    def _ret_pagination_nums(request, item_list):
+        page = request.args.get('page', None)
 
-  TEST: When you click the trash icon next to a question, the question will be removed.
-  This removal will persist in the database and when you refresh the page. 
-  '''
+        if not page:
+            abort(400) # Bad Request
 
-    '''
-  @TODO: 
-  Create an endpoint to POST a new question, 
-  which will require the question and answer text, 
-  category, and difficulty score.
+        start = (page * QUESTIONS_PER_PAGE)
+        end = start + QUESTIONS_PER_PAGE
 
-  TEST: When you submit a question on the "Add" tab, 
-  the form will clear and the question will appear at the end of the last page
-  of the questions list in the "List" tab.  
-  '''
+        if start > len(item_list):
+            abort(422) # Unprocessable Entity
 
-    '''
-  @TODO: 
-  Create a POST endpoint to get questions based on a search term. 
-  It should return any questions for whom the search term 
-  is a substring of the question. 
+        return item_list[start:end]
 
-  TEST: Search by any phrase. The questions list will update to include 
-  only question that include that string within their question. 
-  Try using the word "title" to start. 
-  '''
 
-    '''
-  @TODO: 
-  Create a GET endpoint to get questions based on category. 
+    @app.route('/questions', methods=['GET'])
+    def get_questions():
+        all_questions =
 
-  TEST: In the "List" tab / main screen, clicking on one of the 
-  categories in the left column will cause only questions of that 
-  category to be shown. 
-  '''
+        return jsonify(
+            {
+                'questions': '',
+                'totalQuestions': '',
+                'categories': '',
+                'currentCategory': ''
+            }
+        )
 
-    '''
-  @TODO: 
-  Create a POST endpoint to get questions to play the quiz. 
-  This endpoint should take category and previous question parameters 
-  and return a random questions within the given category, 
-  if provided, and that is not one of the previous questions. 
 
-  TEST: In the "Play" tab, after a user selects "All" or a category,
-  one question at a time is displayed, the user is allowed to answer
-  and shown whether they were correct or not. 
-  '''
 
-    '''
-  @TODO: 
-  Create error handlers for all expected errors 
-  including 404 and 422. 
-  '''
+'''
+@TODO:
+Create an endpoint to handle GET requests for questions,
+including pagination (every 10 questions).
+This endpoint should return a list of questions,
+number of total questions, current category, categories.
 
-    return app
+TEST: At this point, when you start the application
+you should see questions and categories generated,
+ten questions per page and pagination at the bottom of the screen for three pages.
+Clicking on the page numbers should update the questions.
+'''
+
+'''
+@TODO: 
+Create an endpoint to DELETE question using a question ID. 
+
+TEST: When you click the trash icon next to a question, the question will be removed.
+This removal will persist in the database and when you refresh the page. 
+'''
+
+'''
+@TODO: 
+Create an endpoint to POST a new question, 
+which will require the question and answer text, 
+category, and difficulty score.
+
+TEST: When you submit a question on the "Add" tab, 
+the form will clear and the question will appear at the end of the last page
+of the questions list in the "List" tab.  
+'''
+
+'''
+@TODO: 
+Create a POST endpoint to get questions based on a search term. 
+It should return any questions for whom the search term 
+is a substring of the question. 
+
+TEST: Search by any phrase. The questions list will update to include 
+only question that include that string within their question. 
+Try using the word "title" to start. 
+'''
+
+'''
+@TODO: 
+Create a GET endpoint to get questions based on category. 
+
+TEST: In the "List" tab / main screen, clicking on one of the 
+categories in the left column will cause only questions of that 
+category to be shown. 
+'''
+
+'''
+@TODO: 
+Create a POST endpoint to get questions to play the quiz. 
+This endpoint should take category and previous question parameters 
+and return a random questions within the given category, 
+if provided, and that is not one of the previous questions. 
+
+TEST: In the "Play" tab, after a user selects "All" or a category,
+one question at a time is displayed, the user is allowed to answer
+and shown whether they were correct or not. 
+'''
+
+'''
+@TODO: 
+Create error handlers for all expected errors 
+including 404 and 422. 
+'''
+
+return app
