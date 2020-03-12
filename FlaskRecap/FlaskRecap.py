05de94c6f2003c43155acdc71d50c62b04864ec6
Simone Fusco <simone.fusco@ennova.it>
2020-03-12 15:45:38 +0100

    Fixed Auth0 integration

--- FlaskRecap/FlaskRecap.py
+++ FlaskRecap/FlaskRecap.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify, abort
+from functools import wraps
 
 app = Flask(__name__)
 
@@ -31,3 +32,56 @@ def greeting_add():
         abort(422)
     greetings[info['lang']] = info['greeting']
     return jsonify({'greetings':greetings})
+
+# Let's add a simple decorator to inject a greeting
+def add_greeting(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        print("Hello!")
+        return f(*args, **kwargs)
+    return wrapper
+
+@add_greeting
+def print_name(name):
+    print(name)
+
+
+print_name("sandy")
+
+
+
+# Let's add some complexity in the form of a paramater
+def add_greeting(greeting=''):
+    def add_greeting_decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            print(greeting)
+            return f(*args, **kwargs)
+        return wrapper
+    return add_greeting_decorator
+
+@add_greeting("what's up!")
+def print_name(name):
+    print(name)
+
+
+print_name("kathy")
+
+
+# We can also pass information back to the wrapped method
+def add_greeting(greeting=''):
+    def add_greeting_decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            print(greeting)
+            return f(greeting, *args, **kwargs)
+        return wrapper
+    return add_greeting_decorator
+
+@add_greeting("Yo!")
+def print_name(greeting, name):
+    print(greeting)
+    print(name)
+
+
+print_name("Abe")
--- LocalStore/auth0_exercise.py
+++ LocalStore/auth0_exercise.py
@@ -12,6 +12,11 @@ AUTH0_DOMAIN = 'simoennova.eu.auth0.com'
 ALGORITHMS = ['RS256']
 API_AUDIENCE = 'image'
 
+# Link per il Token JWT
+# https://simoennova.eu.auth0.com/authorize?audience=image&response_type=token&client_id=DCpYAsWXwzgmriunr0IvlM8z5g9Vowtt&redirect_uri=http://localhost:8080/login-results
+# Link per il Login
+# https://simoennova.eu.auth0.com/login?state=g6Fo2SBzbUs1Yk9xWVdkSHNMR1lscmV1cy1LX3BtUzlRZ0M5aaN0aWTZIGx4aUhrMkdDV2Z6aENXWnh2YXBiUWFxWDN2NFRVSE5uo2NpZNkgWThOZTRBVFF4TTdsSVBmbVh4VWF4WVdPOHdkTzRKVDE&client=Y8Ne4ATQxM7lIPfmXxUaxYWO8wdO4JT1&protocol=oauth2&prompt=consent&response_type=code&redirect_uri=https%3A%2F%2Fmanage.auth0.com%2Ftester%2Fcallback%3Fconnection%3Dgoogle-oauth2&scope=openid%20profile
+
 '''
 AuthError Exception
 A standardized way to communicate auth failure modes
@@ -23,23 +28,18 @@ class AuthError(Exception):
 
 # PASTE YOUR OWN TOKEN HERE
 # MAKE SURE THIS IS A VALID AUTH0 TOKEN FROM THE LOGIN FLOW
-token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ik56Z3lSVFZEUmpKQ1FrUkJSRGN3TjBReFJUQTFPVUl5UlRORk9EQXhPVGMwTmp"\
-        "jNU9USkVPQSJ9.eyJpc3MiOiJodHRwczovL2ZzbmQuYXV0aDAuY29tLyIsInN1YiI6ImF1dGgwfDVkMDNkM2U2NzI2YjhmMGNiNGJmNzFjOS"\
-        "IsImF1ZCI6ImltYWdlIiwiaWF0IjoxNTYwNTU2MTc0LCJleHAiOjE1NjA1NjMzNzQsImF6cCI6ImtpNEI2alprdUpkODdicEIyTXc4emRraj"\
-        "FsM29mcHpqIiwic2NvcGUiOiIiLCJwZXJtaXNzaW9ucyI6WyJnZXQ6aW1hZ2VzIiwicG9zdDppbWFnZXMiXX0.ENxNT1lo_sX9rpgmGJmiu14"\
-        "lugmYXqb8siJwC1nPuGSb_ycK02KyS5IkA-YkhySMBcDD5IJfawPkJNmJPtUAB1wYVP8hlNsBuvLjtYxzH_VHNeXzVXWQvM7RiuPwrmWJmJN2"\
-        "onmZPh3bjiUZxvyAp0Yp0Rvm54SsiDjO_Dj1Qx-Az_Zjo-mY2ECfFgAo0ifnqDMIgE5YDZ3uOzMni4oEU5Ok-TrQOSwyfJyUC1KQ7ubQ-Bnbh-"\
-        "0Aii9UK9R4JBH7iIMva8_edQkgR4MuRXatYhsqvHsxQ2Iv5rjMmTAmjknsYWE5VYrzafRGVigbPD9A6ELEnyjADBQ9vMtSdPQe2w "
-
+token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik9FSXlNREUxTnpKQk1EZ3pORVpGTkRRNU1rVTFRalpETlRoQ1F6RXdSakJCUXpRMlJVWkRNQSJ9.eyJpc3MiOiJodHRwczovL3NpbW9lbm5vdmEuZXUuYXV0aDAuY29tLyIsInN1YiI6Imdvb2dsZS1vYXV0aDJ8MTAzNTA3NDUwMzk0MTEwNjQ3MDg3IiwiYXVkIjpbImltYWdlIiwiaHR0cHM6Ly9zaW1vZW5ub3ZhLmV1LmF1dGgwLmNvbS91c2VyaW5mbyJdLCJpYXQiOjE1Nzk1NTI1MDksImV4cCI6MTU3OTU1OTcwOSwiYXpwIjoiRENwWUFzV1h3emdtcml1bnIwSXZsTTh6NWc5Vm93dHQiLCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIn0.Poy0W14jz_0zcsNgnEc1mcIqvtd9vn0UIjLRickeYZOrcc44PzJfGnb2l0iOkCRpa9Pk8weuTR-c_PfNwSuPrtjgjLZDWE3BiuuhiGHwsXrkhaLGLtuRMv-U6G48b9OgUqreaPHwRPO0rKqTRgEyJhg1n5MmSO9GEaC-2QbMKEp9tJciWHl8olLQ-ruyMJiTxpcCB268SU3d8ejsdl3xjlwzjyi5bBoLu0DycaFbOqHNWItBouKkLIbU3BR2bkkpgSs9PigNrllLOdrMDKBTubV_xIXwefeNz7Mvd9yixhj2pHmjqYeqo8jxCm3KyKojsiA0QmjzD2zsKs7S11_7Og"
 
 ## Auth Header
 def verify_decode_jwt(token):
     # GET THE PUBLIC KEY FROM AUTH0
     jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
+    print('JSONURL: ' + str(jsonurl))
     jwks = json.loads(jsonurl.read())
 
     # GET THE DATA IN THE HEADER
     unverified_header = jwt.get_unverified_header(token)
+    print('unverified_header: ' + str(unverified_header))
 
     # CHOOSE OUR KEY
     rsa_key = {}
@@ -93,3 +93,7 @@ def verify_decode_jwt(token):
         'code': 'invalid_header',
         'description': 'Unable to find the appropriate key.'
     }, 400)
+
+
+if __name__ == '__main__':
+    verify_decode_jwt(token)
